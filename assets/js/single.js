49ac22cf2824f5d384cf8ceaf21ff9db650f3606
samsiebert <sammsiebert@gmail.com>
2022-06-10 11:37:14 -0500

    connect single and index pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,27 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+
+var getRepoName = function() {
+    //grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+
+    if (repoName) {
+        //display repo name on the page
+      repoName.textContent = repoName;
+
+      getRepoIssues(repoName);
+    } else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+
+    
+    
+}
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -8,12 +31,17 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                //check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } else {
-            alert("There was a problem with your request!");
+            //if not successful, redirect to homepage
+            document.location.replace("./index.html");
         }
     });
-    console.log(repo);
 }
 
 var displayIssues = function(issues) {
@@ -54,4 +82,17 @@ for (var i =0; i < issues.length; i ++) {
 }
 };
 
-getRepoIssues("samsiebert/git-it-done");
+var displayWarning = function(repo) {
+    //add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+var linkEl = document.createElement("a");
+linkEl.textContent = "See More Issues on GitHub.com";
+linkEl.setAttribute("href", "https://githuib.com/" + repo + "/issues");
+linkEl.setAttribute("target", "_blank");
+
+//append to warning contianer 
+limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
